5898a578c8d23049b8710cf80e502ccac5928a06
edzye101 <edzye101@gmail.com>
2020-01-05 13:39:38 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+
+
+
+get '/' do
+  erb :index
+end
+
+get 'index.html' do
+  erb :index
+end
+
+
+get 'about.html' do
+    erb :about
+end
+
+get 'solutions.html'do
+    erb :solutions
+end
+
+get 'portfolio.html' do
+    erb :portfolio
+end
+
+
+
+post '/mail' do
+  require 'pony'
+  person = params
+  name = person["name"]
+  email = person["email"]
+  message = person["message"]
+
+
+  Pony.mail({
+    :to => 'e@chipay.io',
+    :from => email,
+    :subject => "#{name}   has contacted you about Chi.Software",
+    :body => "\n \n #{message} \n sent from #{name}",
+    :via => :smtp,
+    :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+    })
+
+
+
+
+end
